835196136c1db089580da4d468fef880719f637e
yyxxryrx <99161583+yyxxryrx@users.noreply.github.com>
2025-03-08 11:36:25 +0800

    New branch (#2)
    * 更改gitignore文件
    
    * 更改应用图标

---
+++ App.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using WinUIEx;
+using SQLitePCL;
+using System.Linq;
+using Microsoft.UI.Xaml;
+using System.Diagnostics;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
+using AppInstance = Microsoft.Windows.AppLifecycle.AppInstance;
+using LaunchActivatedEventArgs = Microsoft.UI.Xaml.LaunchActivatedEventArgs;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System.IO;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace URL_Opening_Selector
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            Batteries_V2.Init();
+            InitializeComponent();
+            // var mainDispatcherQueue = DispatcherQueue.GetForCurrentThread();
+            // Debug.WriteLine(AppInstance.GetCurrent().GetActivatedEventArgs());
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected async override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            var activationArguments = AppInstance.GetCurrent().GetActivatedEventArgs();
+            Debug.WriteLine("1" + activationArguments);
+            if (Globals.Initialed && activationArguments.Kind == ExtendedActivationKind.Protocol)
+            {
+                OnActivated(activationArguments);
+                return;
+            }
+
+            await Globals.AppConfiguration.InitJson();
+            var exitApplicationCommand = (XamlUICommand)Resources["ExitApplicationCommand"];
+            exitApplicationCommand.ExecuteRequested += async (sender, eventArgs) =>
+            {
+                Debug.WriteLine("Exit Clicked!");
+                TrayIcon.Dispose();
+                await Globals.AppConfiguration.Close();
+                if (SettingWindow is WindowEx window)
+                    window.Close();
+                Exit();
+                Environment.Exit(0);
+            };
+            var showSettingsWindowCommand = (XamlUICommand)Resources["ShowSettingsWindowCommand"];
+            showSettingsWindowCommand.ExecuteRequested += (sender, eventArgs) =>
+            {
+                if (SettingWindow is WindowEx window)
+                    window.Activate();
+                else
+                {
+                    SettingWindow = new Settings();
+                    SettingWindow.Activate();
+                }
+            };
+            var trayIcon = (Resources["TrayIcon"] as H.NotifyIcon.TaskbarIcon)!;
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            var iconPath = Path.Combine(basePath, "Assets", "TrayIcon.ico");
+            trayIcon.IconSource = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png"));
+            trayIcon.Icon = new System.Drawing.Icon(iconPath);
+            trayIcon.ForceCreate();
+            TrayIcon = trayIcon;
+            Globals.Initialed = true;
+            if (activationArguments.Kind == ExtendedActivationKind.Protocol)
+                OnActivated(activationArguments);
+            if (SettingWindow is null)
+                SettingWindow = new Settings();
+        }
+
+        private async void OnActivated(AppActivationArguments args)
+        {
+            try
+            {
+                var protocolArgs = (args.Data as ProtocolActivatedEventArgs)!;
+                var uri = protocolArgs.Uri.AbsoluteUri;
+                Debug.WriteLine($"Uri: {uri}");
+                Debug.WriteLine(Globals.AppConfiguration._db?.ToString() ?? "null");
+                var r = await Globals.AppConfiguration.GetUrlPatterns(uri);
+
+                void ShowDialog(string pattern, bool exist, UrlPattern p = null)
+                {
+                    var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+                    if (!dispatcherQueue.HasThreadAccess) return;
+                    dispatcherQueue.TryEnqueue(() =>
+                    {
+                        Debug.WriteLine($"Pattern: {pattern}, Exist: {exist}");
+                        var window = new MainWindow
+                        {
+                            UrlPattern = p,
+                            Pattern = pattern,
+                            Url = uri,
+                            PatternExist = exist,
+                        };
+                        window.Activate();
+                    });
+                }
+
+                if (r.Count > 0)
+                {
+                    var p = r.First();
+                    switch (p.Method)
+                    {
+                        case UrlPatternMethod.Inquire:
+                            ShowDialog(p.Pattern, true, p);
+                            return;
+                        case UrlPatternMethod.Open:
+                            Util.UrlStartWith(uri,
+                                Globals.AppConfiguration.Configuration.Browsers.First(b => b.Name == p.Browser).Path
+                            );
+                            return;
+                        case UrlPatternMethod.None:
+                            return;
+                        default:
+                            // DllImports.ShowMessageBox("未知的打开方式", "错误", 16);
+                            return;
+                    }
+                }
+
+                switch (Globals.AppConfiguration.Configuration.DefaultMethod)
+                {
+                    case UrlPatternMethod.Inquire:
+                        ShowDialog(uri, false);
+                        break;
+                    case UrlPatternMethod.Open:
+                        if (string.IsNullOrEmpty(Globals.AppConfiguration.Configuration.DefaultBrowser)) break;
+                        Util.UrlStartWith(uri,
+                            Globals.AppConfiguration.Configuration.Browsers.First(b =>
+                                b.Name == Globals.AppConfiguration.Configuration.DefaultBrowser).Path
+                        );
+                        break;
+                    case UrlPatternMethod.None:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // DllImports.ShowMessageBox(ex.Message, "错误", 16);
+            }
+        }
+
+        public static Window SettingWindow;
+        public static H.NotifyIcon.TaskbarIcon TrayIcon { get; set; }
+    }
+}
